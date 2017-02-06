c6864fdfd42d439571a3bdc49c91e6207e97939d
Riccardo Russo <riccardo@Riccardos-MacBook-Pro.local>
2017-02-06 15:19:09 -0300

    New website

--- tmp/js/registrazione.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$.fn.bootstrapSwitch.defaults.onText = 'SI';
-	$.fn.bootstrapSwitch.defaults.offText = 'NO';
-
-	//$("[name='my-checkbox'], [name='checkbox-tos'], [name='checkbox-newsletter']").bootstrapSwitch();
-	$("[name='checkbox_tospp'], [name='checkbox_newsletter']").bootstrapSwitch();
-});
