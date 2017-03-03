765c589eea01d71e413fc50639ec2feceedccddb
Riccardo Russo <riccardo@Riccardos-MacBook-Pro.local>
2017-03-03 16:52:53 -0300

    Footer complete

--- site/assets/functions/generic.js
+++ site/assets/functions/generic.js
@@ -1,2 +1,33 @@
 //var indirizzo = "http://app.edgecowork.com/";
 var indirizzo = "http://127.0.0.1:8000/"
+
+
+var l_button_newsletter;
+$("#subscribe_main").on('submit', function(e){
+	e.preventDefault();
+
+	email = $("#email_footer").val();
+
+	if(email) {
+		btn = $("#button_newsletter")[0];
+		l_button_newsletter = Ladda.create(btn);
+		l_button_newsletter.start();  
+		
+		
+		$.ajax({
+			url : indirizzo + "add_newsletter", 
+			type : "POST", 
+			data: {
+				"email" : email
+			},
+			success : success_newsletter,
+			error : success_newsletter
+		});		
+	}
+	
+});
+
+function success_newsletter() {
+	l_button_newsletter.stop();
+	$("#button_newsletter").html("Gracias!").attr("disabled", "disabled");
+}
